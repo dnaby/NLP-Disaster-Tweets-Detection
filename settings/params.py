819598df67f5880af38219f7d2d519419478d737
acoderwannabe19 <acoderwannabe19>
2024-08-04 11:46:30 +0000

    feat: model exploration

--- settings/params.py
+++ settings/params.py
@@ -1,5 +1,10 @@
 """Project parameters."""
 
 MODEL_PARAMS = {
-    
+    "TARGET_NAME": "target",
+    "MIN_PPS": 0.10,  # Minimal value for Predictive Power Score (PPS)
+
 }
+
+# random seed
+SEED = 42
